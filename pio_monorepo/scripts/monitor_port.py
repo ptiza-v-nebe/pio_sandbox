f1f0a197ff26d98f8f3da44e46976dcf2188e648
Belyaev, Ivan <ivan.belyaev@dlr.de>
2021-10-22 18:41:55 +0200

    updated monitor_script

--- pio_monorepo/scripts/monitor_port.py
+++ pio_monorepo/scripts/monitor_port.py
@@ -1,8 +1,10 @@
 from serial.tools.list_ports import comports
+from platformio.commands.device import helpers as device_helpers
 import sys
 
-serial_number="003700373438510934313939"
+project_options = device_helpers.get_project_options(sys.argv[1])
+serial_number = project_options["custom_hla_serial"]
 
 for i in comports():
-    if i.serial_number==serial_number:
+    if i.serial_number == serial_number:
         sys.stdout.write(i.device)
